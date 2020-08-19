4797638a164a101ad0b9bbe7b6d47a73704bc4d2
zk-wz <60373614+zk-wz@users.noreply.github.com>
2020-08-19 18:23:28 +0800

    修改图片路径

--- random setu-from internet.py
+++ random setu-from internet.py
@@ -12,7 +12,7 @@ bcc = Broadcast(loop=loop)
 app = GraiaMiraiApplication(
     broadcast=bcc,
     connect_info=Session(
-        host="http://0.0.0.0:8080", # 填入 httpapi 服务运行的地址
+        host="http://127.0.0.1:8080", # 填入 httpapi 服务运行的地址
         authKey="123456", # 填入 authKey
         account=123456789, # 你的机器人的 qq 号
         websocket=False # Graia 已经可以根据所配置的消息接收的方式来保证消息接收部分的正常运作.
@@ -23,7 +23,7 @@ app = GraiaMiraiApplication(
 async def group_message_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group):
     if message.asDisplay().startswith("色图"):
         await app.sendGroupMessage(group, MessageChain.create([
-            await Image.fromNetworkAddress('https://cdn.jsdelivr.net/gh/zk-wz/randomsetu/piccache/  ('+str(random.randrange(1,302))+').jpg')
+            await Image.fromNetworkAddress('https://cdn.jsdelivr.net/gh/zk-wz/randomsetu/piccache/  ('+str(random.randrange(1,101))+').jpg')
         ]))
 
 
