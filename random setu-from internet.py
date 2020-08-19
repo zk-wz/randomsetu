8022d9c765ec41fad6bd605b10c41be7b30055ce
zk-wz <60373614+zk-wz@users.noreply.github.com>
2020-08-19 18:15:41 +0800

    修改websocket

--- random setu-from internet.py
+++ random setu-from internet.py
@@ -15,7 +15,7 @@ app = GraiaMiraiApplication(
         host="http://0.0.0.0:8080", # 填入 httpapi 服务运行的地址
         authKey="123456", # 填入 authKey
         account=123456789, # 你的机器人的 qq 号
-        websocket=True # Graia 已经可以根据所配置的消息接收的方式来保证消息接收部分的正常运作.
+        websocket=False # Graia 已经可以根据所配置的消息接收的方式来保证消息接收部分的正常运作.
     )
 )
 
